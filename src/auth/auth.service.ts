3d3f0dacaabc19987eee1ea0edb9876e60a0a611
hodoodang <xogh4096@gmail.com>
2022-03-18 23:13:41 +0900

    add argon2 package for password encryption

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
     constructor(private prisma: PrismaService) {}
 
-    signup() {
-        return { msg: 'I have signed up' };
+    async signup(dto: CreateAuthDto) {
+
+        const hash = await argon.hash(dto.password);
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+        });
+        return user;
     }
 
     signin() {
