255479b3f8e7ec0fee6a4987011d4dbafad46f48
hodoodang <xogh4096@gmail.com>
2022-03-25 22:51:02 +0900

    add using Guard code

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,48 +1,13 @@
-import {
-    Controller,
-    Get,
-    Post,
-    Body,
-    Patch,
-    Param,
-    Delete,
-    HttpCode,
-    BadRequestException,
-} from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from '../auth/guard';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
-
-    @Post()
-    create(@Body() createUserDto: CreateUserDto) {
-        return this.usersService.createUser(createUserDto);
-    }
-
-    @Get()
-    findAll() {
-        return this.usersService.findAll();
-    }
-
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        if (+id < 1) {
-            throw new BadRequestException('id는 0보다 큰 값이어야 합니다.');
-        }
-        return this.usersService.findOne(+id);
-    }
-
-    @HttpCode(202)
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-        return this.usersService.updateUser();
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.usersService.deleteUser();
+    @UseGuards(JwtGuard)
+    @Get('me')
+    getMe(@Req() req: Request) {
+        return req.user;
     }
 }
