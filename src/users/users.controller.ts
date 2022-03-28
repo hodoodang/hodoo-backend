bf44f0e0f003886691ee8059779ab31559650f6b
hodoodang <xogh4096@gmail.com>
2022-03-28 23:47:52 +0900

    add decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { User } from '@prisma/client';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private userService: UsersService) {}
+
     @UseGuards(JwtGuard)
-    @Get('me')
-    getMe(@Req() req: Request) {
-        return req.user;
+    @Get('/:id')
+    getMe(@GetUser() user: User) {
+        return user;
+    }
+
+    @UseGuards(JwtGuard)
+    @Patch('/:id')
+    editUser(@GetUser() user: User) {
+        return this.userService.updateUser({
+            where: user,
+            data: user,
+        });
     }
 }
